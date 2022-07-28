ca5062641551a883e3af3f1cbfc782ce72204717
hoferbit <hoferbao@gmail.com>
2022-07-28 17:53:17 +0800

    python101

---
+++ python101/app.py
@@ -0,0 +1,9 @@
+print("Hello Hofer")
+print('*' * 5)  # *****
+
+birth_year = input('Birth year: ')
+age = 2022 - int(birth_year)
+print(type(age))
+print(age)
+
+
---
+++ python101/string.py
@@ -0,0 +1,14 @@
+course = ''' 1
+line1
+line2
+ 2
+'''
+
+print(course)
+
+course = 'Python for Beginners'
+print(course[0:1])
+print(course[:])
+
+print(course.upper())
+
